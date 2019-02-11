2155acec07af9e3a4550c2d5a20c7f945a8fad6b
Malte <SkyGuardian42@gmail.com>
2019-02-11 23:29:08 +0100

    Initial Working

---
+++ index.js
@@ -0,0 +1,20 @@
+const countdown = require('countdown');
+
+countdown.setLabels(
+	' Millisekunde| Sekunde| Minute| Stunde| Tag| Woche| Monat| année| décennie| siècle| millénaire',
+	' Millisekunden| Sekunden| Minuten| Stunden| Tage| Wochen| Monate| années| décennies| siècles| millénaires',
+	', ',
+	', ',
+	'BEENDET');
+
+var timerId =
+  countdown(
+    new Date(2019, 2, 9, 19, 30),
+    (ts) => {
+
+			if(ts.value < 0)
+				document.querySelector('#pageTimer').innerHTML =  "Tensing in<br>" + ts.toString().replace(/,/g, "<br>");
+    	else
+				document.querySelector('#pageTimer').innerHTML = "Die <br>TENSING<br> Show<br> findet<br>statt"
+		},
+		countdown.MONTHS|countdown.WEEKS|countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
